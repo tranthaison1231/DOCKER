7affc1d627081a7feef0f3fb6c477e6a73ee4de2
tranthaison1231 <43759215+tranthaison1231@users.noreply.github.com>
2020-04-07 17:44:53 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,8 @@
 echo "Setting docker..." \
 && cd ~ \
-&& sudo su - \
-&& yum update -y \
-&& yum install -y docker \
-&& service docker start \
-&& usermod -a -G docker ec2-user \
-&& curl -L "https://github.com/docker/compose/rel... -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
-&& chmod +x /usr/local/bin/docker-compose
+&& sudo yum update -y \
+&& sudo yum install -y docker \
+&& sudo service docker start \
+&& sudo usermod -a -G docker ec2-user \
+&& sudo curl -L "https://github.com/docker/compose/rel... -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+&& sudo chmod +x /usr/local/bin/docker-compose
